ee818b86788eb2cbd23b6948f6325fc5db618967
joao garcia <jiboiadamor@gmail.com>
2022-12-12 13:34:07 +0000

    display image on DOM

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 function onSubmit(e) {
   e.preventDefault();
 
+  document.querySelector('.msg').textContent = '';
+  document.querySelector('#image').src = '';
+
   const prompt = document.querySelector('#prompt').value;
   const size = document.querySelector('#size').value;
 
@@ -33,7 +36,11 @@ async function generateImageRequest(prompt, size) {
     }
 
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+
+    const imageUrl = data.data;
+
+    document.querySelector('#image').src = imageUrl;
 
     removeSpinner();
   } catch (error) {
